1e0d68bc615f5c01164960238e39d010e4249bca
Sam Laye <layesamss97@gmail.com>
2024-02-02 21:10:22 +0000

    Margin top au niveau du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
+    <footer className="mt-4">
       <p>
         2018 Reduction theme, source on <a href="#">Github</a>
       </p>
